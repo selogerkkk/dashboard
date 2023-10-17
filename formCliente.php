1581f631dfd9920fba034bfa089b86ac3101a90a
selogerkkk <ruanhborges@gmail.com>
2023-10-17 09:42:36 -0300

    add: criaçao forms cliente

---
+++ formCliente.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'header.php'; ?>
+</head>
+
+<body>
+    <div class="wrapper">
+        <?php include 'menu.php'; ?>
+
+        <div class="main">
+            <?php include 'topmenu.php'; ?>
+
+
+            <footer class="footer">
+                <?php include 'footer.php'; ?>
+            </footer>
+        </div>
+    </div>
+
+    <script src="js/app.js"></script>
+</body>
+
+</html>
