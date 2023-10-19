fa04765ca0618aa9e4bcf611963d6a93773fbcfc
selogerkkk <ruanhborges@gmail.com>
2023-10-19 09:56:46 -0300

    add: jquery cdn

--- footer.php
+++ footer.php
@@ -27,3 +27,5 @@
         </div>
     </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
