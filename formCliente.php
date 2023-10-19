0f9e4ea6ef8cc55440017f4f246fc9d579a9c9b4
selogerkkk <ruanhborges@gmail.com>
2023-10-19 11:10:35 -0300

    add: campo bairro

--- formCliente.php
+++ formCliente.php
@@ -24,39 +24,32 @@
                                 <form action="#">
                                     <div class="mb-3">
                                         <label for="exampleFormControlInput1" class="form-label">Nome completo</label>
-                                        <input type="text" class="form-control" id="exampleFormControlInput1" required
-                                            autocomplete="off">
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" required autocomplete="off">
                                     </div>
                                     <div class="mb-3">
                                         <label for="exampleFormControlInput1" class="form-label">E-mail</label>
-                                        <input type="email" class="form-control" id="exampleFormControlInput1" required
-                                            autocomplete="off">
+                                        <input type="email" class="form-control" id="exampleFormControlInput1" required autocomplete="off">
                                     </div>
                                     <div class="mb-3">
                                         <label for="exampleFormControlInput1" class="form-label">Telefone</label>
-                                        <input type="text" class="form-control" id="exampleFormControlInput1" required
-                                            autocomplete="off">
+                                        <input type="text" class="form-control" id="telefone" name="telefone" required autocomplete="off">
                                     </div>
                                     <div class="mb-3">
                                         <label for="exampleFormControlInput1" class="form-label">CPF/CNPJ</label>
-                                        <input type="text" class="form-control" id="exampleFormControlInput1" required
-                                            autocomplete="off">
+                                        <input type="text" class="form-control" id="cpfcnpj" required autocomplete="off">
                                     </div>
 
                                     <div class="row">
-                                        <div class="col-3">
+                                        <div class="col-4">
                                             <div class="mb-3">
                                                 <label for="exampleFormControlInput1" class="form-label">CEP</label>
-                                                <input type="text" class="form-control" id="exampleFormControlInput1"
-                                                    required autocomplete="off">
+                                                <input type="text" class="form-control" id="cep" maxlength="8" required autocomplete="off">
                                             </div>
                                         </div>
-                                        <div class="col-9">
+                                        <div class="col-8">
                                             <div class="mb-3">
-                                                <label for="exampleFormControlInput1"
-                                                    class="form-label">Logradouro</label>
-                                                <input type="text" class="form-control" id="exampleFormControlInput1"
-                                                    required autocomplete="off">
+                                                <label for="exampleFormControlInput1" class="form-label">Logradouro</label>
+                                                <input type="text" class="form-control" id="logradouro" required autocomplete="off">
                                             </div>
                                         </div>
                                     </div>
@@ -64,23 +57,35 @@
                                         <div class="col-3">
                                             <div class="mb-3">
                                                 <label for="exampleFormControlInput1" class="form-label">Nº</label>
-                                                <input type="text" class="form-control" id="exampleFormControlInput1"
-                                                    required autocomplete="off">
+                                                <input type="text" class="form-control" id="exampleFormControlInput1" required autocomplete="off">
                                             </div>
                                         </div>
-                                        <div class="col-6">
+                                        <div class="col-9">
                                             <div class="mb-3">
-                                                <label for="exampleFormControlInput1" class="form-label">Cidade</label>
-                                                <input type="text" class="form-control" id="exampleFormControlInput1"
-                                                    required autocomplete="off">
+                                                <label for="exampleFormControlInput1" class="form-label">Complemento</label>
+                                                <input type="text" class="form-control" id="complemento" required autocomplete="off">
                                             </div>
                                         </div>
-                                        <div class="col-3">
-                                            <div class="mb-3">
-                                                <label for="exampleFormControlInput1" class="form-label">Estado</label>
-                                                <input type="text" class="form-control" id="exampleFormControlInput1"
-                                                    required autocomplete="off">
+                                        <div class="row">
+                                            <div class="col-5">
+                                                <div class="mb-3">
+                                                    <label for="exampleFormControlInput1" class="form-label">Bairro</label>
+                                                    <input type="text" class="form-control" id="bairro" required autocomplete="off">
+                                                </div>
                                             </div>
+                                            <div class="col-5">
+                                                <div class="mb-3">
+                                                    <label for="exampleFormControlInput1" class="form-label">Cidade</label>
+                                                    <input type="text" class="form-control" id="cidade" required autocomplete="off">
+                                                </div>
+                                            </div>
+                                            <div class="col-2">
+                                                <div class="mb-3">
+                                                    <label for="exampleFormControlInput1" class="form-label">Estado</label>
+                                                    <input type="text" class="form-control" id="uf" required autocomplete="off">
+                                                </div>
+                                            </div>
+
                                         </div>
                                     </div>
                                     <div class="mb-3">
@@ -106,3 +111,6 @@
 </body>
 
 </html>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/0.9.0/jquery.mask.min.js" integrity="sha512-oJCa6FS2+zO3EitUSj+xeiEN9UTr+AjqlBZO58OPadb2RfqwxHpjTU8ckIC8F4nKvom7iru2s8Jwdo+Z8zm0Vg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
