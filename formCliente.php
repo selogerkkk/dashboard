495ffb19c17dc71994536244a4ad8765e9ddaac9
selogerkkk <ruanhborges@gmail.com>
2023-10-20 09:53:21 -0300

    fix: usando javascript como seletor inves de jquery

--- formCliente.php
+++ formCliente.php
@@ -24,39 +24,32 @@
                                 <form action="#">
                                     <div class="mb-3">
                                         <label for="exampleFormControlInput1" class="form-label">Nome completo</label>
-                                        <input type="text" class="form-control" id="exampleFormControlInput1" required
-                                            autocomplete="off">
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" required autocomplete="off">
                                     </div>
                                     <div class="mb-3">
                                         <label for="exampleFormControlInput1" class="form-label">E-mail</label>
-                                        <input type="email" class="form-control" id="exampleFormControlInput1" required
-                                            autocomplete="off">
+                                        <input type="email" class="form-control" id="exampleFormControlInput1" required autocomplete="off">
                                     </div>
                                     <div class="mb-3">
                                         <label for="exampleFormControlInput1" class="form-label">Telefone</label>
-                                        <input type="text" class="form-control" id="telefone" name="telefone" required
-                                            autocomplete="off">
+                                        <input type="text" class="form-control" id="telefone" name="telefone" required autocomplete="off">
                                     </div>
                                     <div class="mb-3">
                                         <label for="exampleFormControlInput1" class="form-label">CPF/CNPJ</label>
-                                        <input type="text" class="form-control" id="cpfcnpj" required
-                                            autocomplete="off">
+                                        <input type="text" class="form-control" id="cpfcnpj" required autocomplete="off">
                                     </div>
 
                                     <div class="row">
                                         <div class="col-4">
                                             <div class="mb-3">
                                                 <label for="exampleFormControlInput1" class="form-label">CEP</label>
-                                                <input type="text" class="form-control" id="cep" maxlength="8" required
-                                                    autocomplete="off">
+                                                <input type="text" class="form-control" id="cep" maxlength="8" required autocomplete="off">
                                             </div>
                                         </div>
                                         <div class="col-8">
                                             <div class="mb-3">
-                                                <label for="exampleFormControlInput1"
-                                                    class="form-label">Logradouro</label>
-                                                <input type="text" class="form-control" id="logradouro" required
-                                                    autocomplete="off">
+                                                <label for="exampleFormControlInput1" class="form-label">Logradouro</label>
+                                                <input type="text" class="form-control" id="logradouro" required autocomplete="off">
                                             </div>
                                         </div>
                                     </div>
@@ -64,41 +57,32 @@
                                         <div class="col-3">
                                             <div class="mb-3">
                                                 <label for="exampleFormControlInput1" class="form-label">Nº</label>
-                                                <input type="text" class="form-control" id="exampleFormControlInput1"
-                                                    required autocomplete="off">
+                                                <input type="text" class="form-control" id="exampleFormControlInput1" required autocomplete="off">
                                             </div>
                                         </div>
                                         <div class="col-9">
                                             <div class="mb-3">
-                                                <label for="exampleFormControlInput1"
-                                                    class="form-label">Complemento</label>
-                                                <input type="text" class="form-control" id="complemento" required
-                                                    autocomplete="off">
+                                                <label for="exampleFormControlInput1" class="form-label">Complemento</label>
+                                                <input type="text" class="form-control" id="complemento" required autocomplete="off">
                                             </div>
                                         </div>
                                         <div class="row">
                                             <div class="col-5">
                                                 <div class="mb-3">
-                                                    <label for="exampleFormControlInput1"
-                                                        class="form-label">Bairro</label>
-                                                    <input type="text" class="form-control" id="bairro" required
-                                                        autocomplete="off">
+                                                    <label for="exampleFormControlInput1" class="form-label">Bairro</label>
+                                                    <input type="text" class="form-control" id="bairro" required autocomplete="off">
                                                 </div>
                                             </div>
                                             <div class="col-5">
                                                 <div class="mb-3">
-                                                    <label for="exampleFormControlInput1"
-                                                        class="form-label">Cidade</label>
-                                                    <input type="text" class="form-control" id="cidade" required
-                                                        autocomplete="off">
+                                                    <label for="exampleFormControlInput1" class="form-label">Cidade</label>
+                                                    <input type="text" class="form-control" id="cidade" required autocomplete="off">
                                                 </div>
                                             </div>
                                             <div class="col-2">
                                                 <div class="mb-3">
-                                                    <label for="exampleFormControlInput1"
-                                                        class="form-label">Estado</label>
-                                                    <input type="text" class="form-control" id="uf" required
-                                                        autocomplete="off">
+                                                    <label for="exampleFormControlInput1" class="form-label">Estado</label>
+                                                    <input type="text" class="form-control" id="uf" required autocomplete="off">
                                                 </div>
                                             </div>
 
@@ -129,59 +113,56 @@
 </html>
 
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/0.9.0/jquery.mask.min.js"
-    integrity="sha512-oJCa6FS2+zO3EitUSj+xeiEN9UTr+AjqlBZO58OPadb2RfqwxHpjTU8ckIC8F4nKvom7iru2s8Jwdo+Z8zm0Vg=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/0.9.0/jquery.mask.min.js" integrity="sha512-oJCa6FS2+zO3EitUSj+xeiEN9UTr+AjqlBZO58OPadb2RfqwxHpjTU8ckIC8F4nKvom7iru2s8Jwdo+Z8zm0Vg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script>
-$("#cep").on("input", function() {
-    var numCep = $("#cep").val();
-
-    if (numCep.length >= 7) {
-        viaCEP();
-    }
-});
-
-function viaCEP() {
-    var numCep = $("#cep").val();
-    var url = "https://viacep.com.br/ws/" + numCep + "/json";
-
-    $.ajax({
-        url: url,
-        type: "get",
-        dataType: "json",
-        success: function(dados) {
-            $("#uf").val(dados.uf);
-            $("#cidade").val(dados.localidade);
-            $("#logradouro").val(dados.logradouro);
-            $("#bairro").val(dados.bairro);
+    document.getElementById("cep").addEventListener("input", function() {
+        var numCep = document.getElementById("cep").value;
+        if (numCep >= 7) {
+            viaCEP();
         }
-    })
-}
+    });
+
+    function viaCEP() {
+        var numCep = $("#cep").val();
+        var url = "https://viacep.com.br/ws/" + numCep + "/json";
+
+        $.ajax({
+            url: url,
+            type: "get",
+            dataType: "json",
+            success: function(dados) {
+                $("#uf").val(dados.uf);
+                $("#cidade").val(dados.localidade);
+                $("#logradouro").val(dados.logradouro);
+                $("#bairro").val(dados.bairro);
+            }
+        })
+    }
 </script>
 
 <script type="text/javascript">
-$("#telefone, #celular").mask("(00) 00000-0000");
-$("#cpfcnpj").keydown(function() {
-    try {
-        $("#cpfcnpj").unmask();
-    } catch (e) {}
+    $("#telefone, #celular").mask("(00) 00000-0000");
+    $("#cpfcnpj").keydown(function() {
+        try {
+            $("#cpfcnpj").unmask();
+        } catch (e) {}
 
-    var tamanho = $("#cpfcnpj").val().length;
+        var tamanho = $("#cpfcnpj").val().length;
 
-    if (tamanho < 14) {
-        $("#cpfcnpj").mask("999.999.999-99");
-    } else {
-        $("#cpfcnpj").mask("99.999.999/9999-99");
-    }
+        if (tamanho < 14) {
+            $("#cpfcnpj").mask("999.999.999-99");
+        } else {
+            $("#cpfcnpj").mask("99.999.999/9999-99");
+        }
 
-    var elem = this;
-    setTimeout(function() {
-        elem.selectionStart = elem.selectionEnd = 10000;
-    }, 0);
+        var elem = this;
+        setTimeout(function() {
+            elem.selectionStart = elem.selectionEnd = 10000;
+        }, 0);
 
-    var currentValue = $(this).val();
-    $(this).val('');
-    $(this).val(currentValue);
-});
+        var currentValue = $(this).val();
+        $(this).val('');
+        $(this).val(currentValue);
+    });
 </script>
