e9f0fa50e04719d422016f704b660e9d79699d39
selogerkkk <ruanhborges@gmail.com>
2023-10-26 12:06:31 -0300

    feat: atualizaçao de dados de cliente

---
+++ atualizarCliente.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'config.php';
+$id = $_POST['id'];
+$nome = $_POST['nome'];
+$mail = $_POST['email'];
+$telefone = $_POST['telefone'];
+$estado = $_POST['uf'];
+
+print $id;
+$sql = "UPDATE `cliente` SET 
+        `nome` = '$nome',
+        `email` = '$mail',
+        `telefone` = '$telefone',
+        `estado` = '$estado'
+        WHERE `id_cliente` = $id";
+
+
+$result = mysqli_query($conn, $sql);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+
+header('location: formCliente.php');
