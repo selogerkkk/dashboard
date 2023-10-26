5de4cea1fcfc721ed2ee5badbda5cb3ccb3966b2
selogerkkk <ruanhborges@gmail.com>
2023-10-26 10:22:26 -0300

    feat: tratamento de erro na conexao do banco

--- config.php
+++ config.php
@@ -6,3 +6,6 @@ $pass = 'password';
 $base = 'phpdashboard';
 
 $conn = mysqli_connect($server, $user, $pass, $base);
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
