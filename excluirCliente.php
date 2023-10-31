e47d3623b6aab6f04a3621d0e6907dd36920ac8f
selogerkkk <ruanhborges@gmail.com>
2023-10-31 09:42:32 -0300

    feat: conexao com o banco para exclusao

---
+++ excluirCliente.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'config.php';
+$id = $_POST['id'];
+
+$sql = "DELETE FROM `cliente` WHERE `id_cliente` = $id";
+
+$result = mysqli_query($conn, $sql);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+
+
+header('location: formCliente.php');
