1f21b950142f412703d97d8d9edfc8b28e6b3fb6
selogerkkk <ruanhborges@gmail.com>
2023-11-01 10:32:42 -0300

    refactor: Remover verificação de conexão no código de atualização de cliente

--- excluirCliente.php
+++ excluirCliente.php
@@ -5,12 +5,6 @@ $id = $_POST['id'];
 
 $sql = "DELETE FROM `cliente` WHERE `id_cliente` = $id";
 
-print $id;
 $result = mysqli_query($conn, $sql);
-
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
-}
-
 
 header('location: formCliente.php');
