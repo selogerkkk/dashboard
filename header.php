184fd8166ddb0c7c6e7faaec327cb50d7614c4c1
selogerkkk <ruanhborges@gmail.com>
2023-11-01 10:42:14 -0300

    style(html): Atualizar título da página
    Esta alteração atualiza o título da página HTML para 'Admin Dashboard'.

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 
 <link rel="canonical" href="https://demo-basic.adminkit.io/" />
 
-<title>AdminKit Demo - Bootstrap 5 Admin Template</title>
+<title>Admin Dashboard</title>
 
 <link href="css/app.css" rel="stylesheet" />
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet" />
