a58916ad24732c5bb86091dd79ed8829cdce94fb
selogerkkk <ruanhborges@gmail.com>
2023-11-01 11:35:20 -0300

    feat: atualizaçao de imagem do cliente no modal de ediçao

--- atualizarCliente.php
+++ atualizarCliente.php
@@ -6,13 +6,32 @@ $nome = $_POST['nome'];
 $mail = $_POST['email'];
 $telefone = $_POST['telefone'];
 $estado = $_POST['estado'];
+$arquivo = $_FILES['fotoatualizada'];
 
-$sql = "UPDATE `cliente` SET 
+
+if ($arquivo !== null) {
+    preg_match("/\.(png|jpg|jpeg){1}$/i", $arquivo["name"], $ext);
+    if ($ext == true) {
+        $nome_arquivo = md5(uniqid(time())) . "." . $ext[1];
+        $caminho_arquivo = "imagens/" . $nome_arquivo;
+        copy($arquivo['tmp_name'], $caminho_arquivo);
+        $sql = "UPDATE `cliente` SET 
+        `nome` = '$nome',
+        `email` = '$mail',
+        `telefone` = '$telefone',
+        `estado` = '$estado',
+        `imagem` = '$nome_arquivo'
+        WHERE `id_cliente` = $id";
+        $result = mysqli_query($conn, $sql);
+    } else {
+        $sql = "UPDATE `cliente` SET 
         `nome` = '$nome',
         `email` = '$mail',
         `telefone` = '$telefone',
         `estado` = '$estado'
         WHERE `id_cliente` = $id";
-$result = mysqli_query($conn, $sql);
+        $result = mysqli_query($conn, $sql);
+    }
+}
 
 header('location: formCliente.php');
